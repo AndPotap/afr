bc7f4b37e93a53b204dd984979d7633e8916a479
shikai <sq2129@nyu.edu>
2023-06-12 19:03:13 -0400

    fixed torch version

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,4 +1,4 @@
-python3 -m pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu118 --no-cache-dir
+python3 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 python3 -m pip install matplotlib --no-cache-dir
 python3 -m pip install transformers --no-cache-dir
 python3 -m pip install pandas --no-cache-dir
@@ -6,3 +6,4 @@ python3 -m pip install timm --no-cache-dir
 python3 -m pip install einops --no-cache-dir
 python3 -m pip install wandb --no-cache-dir
 python3 -m pip install wilds
+python3 -m pip install torch-scatter -f https://data.pyg.org/whl/torch-2.0.0+cu118.html --no-cache-dir
